49e93df776a32d8534fd2cd724bffca0208f8ea1
Myles Braithwaite <me@mylesbraithwaite.com>
2023-02-18 10:21:06 -0500

    :construction: Started exploring Yelp's dataset.

---
+++ wip/yelp-dataset-exploration/shared.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).parent
+DATA_PATH = ROOT_PATH / "data"
+DATA_INTERIM_PATH = DATA_PATH / "interim"
+DATA_OUTPUT_PATH = DATA_PATH / "output"
+DATA_SOURCE_PATH = DATA_PATH / "source"
